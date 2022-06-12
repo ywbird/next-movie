aeaff4064d307f6c798c25378dff4ce366c3d8d6
ywbird <repack.lab.io@gmail.com>
2022-06-12 12:33:00 +0900

    redirects, rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = "ffbb1e39db3dd7253121c6925c0bd31a";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old/:path/:date",
+        destination: "/new/:date/:path",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "ffbb1e39db3dd7253121c6925c0bd31a";
+const API_KEY = process.env.API_KEY;
 interface Movie {
   adult: boolean;
   backdrop_path: string;
@@ -21,23 +21,46 @@ interface Movie {
 
 export default function Home(): JSX.Element {
   const [movies, setMovies] = useState<Movie[]>([]);
-  useEffect(() => {}, []);
-  (async () => {
-    const { results }: { results: Movie[] } = await (
-      await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      )
-    ).json();
-    setMovies(results);
-  })();
+  useEffect(() => {
+    (async () => {
+      const { results }: { results: Movie[] } = await (
+        await fetch("/api/movies")
+      ).json();
+      setMovies(results);
+      // console.log(results);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
